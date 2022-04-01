f530b24b4ed190ff077a351d15551f2b131cea6d
cristiany <crisdam1840@gmail.com>
2022-04-01 17:18:27 -0500

    fatal: destination path 'CurseJava' already exists and is not an empty directory

--- src/model/motocicle.java
+++ src/model/motocicle.java
@@ -1,4 +1,26 @@
 package model;
 
-public class motocicle {
+public class motocicle extends Vehicle {
+    private int cylinderization;
+
+    public motocicle(String brand, String reference, Integer mileage, String model, double price, Integer availableQuantity, int cylinderization) {
+        super(brand, reference, mileage, model, price, availableQuantity);
+        this.cylinderization = cylinderization;
+    }
+
+    public int getCylinderization() {
+        return cylinderization;
+    }
+
+    public void setCylinderization(int cylinderization) {
+        this.cylinderization = cylinderization;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        final java.lang.StringBuilder sb = new java.lang.StringBuilder("motocicle{");
+        sb.append("cylinderization=").append(cylinderization);
+        sb.append('}');
+        return sb.toString();
+    }
 }
